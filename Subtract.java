c430d8945e3ef4ac5c2f4c125b0e11f0a579910e
Your Name <youremail@example.com>
2026-01-13 15:55:05 +0530

    Conflicts

---
+++ Subtract.java
@@ -0,0 +1,9 @@
+public class Subtract {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+    }
+    
+}
