6927fcba5a3a9fc3ed3201d86dd6ad72c4981247
“Ankit <ankitchaudhary5168033@gmail.com>
2026-01-13 15:44:51 +0530

    d

---
+++ ThreeNos.java
@@ -0,0 +1,9 @@
+public class ThreeNos {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int sum = a + b + c;
+        System.out.println("The sum of " + a + ", " + b + ", and " + c + " is: " + sum);
+    }
+}
